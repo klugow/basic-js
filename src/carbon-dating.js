f8ffa66e7ed96161fbb394d675b3def31dcd595c
Andrey Makarenko <119301983+klugow@users.noreply.github.com>
2023-04-17 23:22:11 +0300

    new js code write

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+let res = false;
+if (typeof sampleActivity === 'string'
+&& isFinite(sampleActivity)) {
+let active = parseFloat(sampleActivity);
+if (active > 0 && active < MODERN_ACTIVITY) {
+zeit = Math.log(MODERN_ACTIVITY / active) / (0.693 / HALF_LIFE_PERIOD);
+res = Math.ceil(zeit);
+    }
+  }
+
+  return res;
 }
+
 
 module.exports = {
   dateSample
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(arr) {
+  let number = 0;
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      number += countCats(item);
+    } else {
+      if (item === '^^') {
+        number++;
+      }
+    }
+  }
+
+  return number;
 }
 
 module.exports = {
