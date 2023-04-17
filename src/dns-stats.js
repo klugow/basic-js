c7ec570df8675e071ba755e941600447245c80a2
Andrey Makarenko <119301983+klugow@users.noreply.github.com>
2023-04-17 23:36:38 +0300

    new js code write

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const mapppy = new Map();
+
+  for (let domain of domains) {
+    let string = '';
+    let array = domain.split('.').reverse();
+
+    for (let z of array) {
+      string = `${string}.${z}`;
+      if (mapppy.has(string)) {
+        mapppy.set(string, mapppy.get(string) + 1);
+      } else {
+        mapppy.set(string, 1);
+      }
+    }
+  }
+
+  let resualt = {};
+  for (let entry of mapppy) {
+    resualt[entry[0]] = entry[1];
+  }
+
+  return resualt;
 }
 
 module.exports = {
